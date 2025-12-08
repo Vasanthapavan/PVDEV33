16aa6c9914e05b09f3adfb26323f1600a44452f3
Vasanthampavan <pavan@apostletech.com>
2025-12-08 23:14:17 +0530

    Yacht Section added

---
+++ PvkYacht/Schemas/PvkYacht_Detail/PvkYacht_Detail.js
@@ -0,0 +1,8 @@
+define('PvkYacht_Detail', [], function () {
+	return {
+		entitySchemaName: 'PvkYacht',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
